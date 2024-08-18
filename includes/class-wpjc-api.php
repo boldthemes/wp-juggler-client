fe9a059547cd269cb7f487c56f93982c98afbb8d
mackovic <rmackovic@gmail.com>
2024-08-18 09:33:48 +0200

    site health sorted out

--- includes/class-wpjc-api.php
+++ includes/class-wpjc-api.php
@@ -105,6 +105,13 @@ class WPJC_Api
 			'args' => array(),
 			'permission_callback' => array($this, 'api_validate_api_key')
 		));
+
+		register_rest_route('juggler/v1', '/siteHealth/', array(
+			'methods' => 'POST',
+			'callback' => array($this, 'site_health'),
+			'args' => array(),
+			'permission_callback' => array($this, 'api_validate_api_key')
+		));
 	}
 
 	public function test_connection(WP_REST_Request $request)
@@ -255,4 +262,99 @@ class WPJC_Api
 
 		wp_send_json_success($data, 200);
 	}
+
+	public function site_health(WP_REST_Request $request)
+	{
+
+		$health_check_site_status = new WPJC_Health();
+
+		$data = $health_check_site_status->wpjc_health_info();
+
+		wp_send_json_success($data, 200);
+	}
 }
+
+if ( ! class_exists( 'WP_Site_Health' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/class-wp-site-health.php';
+}
+
+class WPJC_Health extends WP_Site_Health{
+
+	public function wpjc_perform_test( $callback ) {
+		return apply_filters( 'site_status_test_result', call_user_func( $callback ) );
+	}
+
+	public function wpjc_health_info(){
+
+		if (! function_exists('get_core_updates')) {
+			require_once ABSPATH . 'wp-admin/includes/update.php';
+		}
+		if (! function_exists('get_plugins')) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+		if (! function_exists('wp_check_php_version')) {
+			require_once ABSPATH . 'wp-admin/includes/misc.php';
+		}
+
+
+		$health_check_js_variables = array(
+			'nonce'       => array(
+				'site_status'        => wp_create_nonce( 'health-check-site-status' ),
+				'site_status_result' => wp_create_nonce( 'health-check-site-status-result' ),
+			),
+			'site_status' => array(
+				'direct' => array(),
+				'async'  => array(),
+				'issues' => array(
+					'good'        => 0,
+					'recommended' => 0,
+					'critical'    => 0,
+				),
+			),
+		);
+
+		$issue_counts = get_transient( 'health-check-site-status-result' );
+
+		if ( false !== $issue_counts ) {
+			$issue_counts = json_decode( $issue_counts );
+
+			$health_check_js_variables['site_status']['issues'] = $issue_counts;
+		}
+
+		$tests = WPJC_Health::get_tests();
+
+			foreach ( $tests['direct'] as $test ) {
+				if ( is_string( $test['test'] ) ) {
+					$test_function = sprintf(
+						'get_test_%s',
+						$test['test']
+					);
+
+					if ( method_exists( $this, $test_function ) && is_callable( array( $this, $test_function ) ) ) {
+						$health_check_js_variables['site_status']['direct'][] = $this->wpjc_perform_test( array( $this, $test_function ) );
+						continue;
+					}
+				}
+
+				if ( is_callable( $test['test'] ) ) {
+					$health_check_js_variables['site_status']['direct'][] = $this->wpjc_perform_test( $test['test'] );
+				}
+			}
+
+			foreach ( $tests['async'] as $test ) {
+				if ( is_string( $test['test'] ) ) {
+					$health_check_js_variables['site_status']['async'][] = array(
+						'test'      => $test['test'],
+						'has_rest'  => ( isset( $test['has_rest'] ) ? $test['has_rest'] : false ),
+						'completed' => false,
+						'headers'   => isset( $test['headers'] ) ? $test['headers'] : array(),
+					);
+				}
+			}
+
+		return $health_check_js_variables;
+	}
+
+}
+
+
