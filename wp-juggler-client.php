ac1cdae644e3491423c4dcac5ea9d9abccd45e26
rmackovic <rmackovic@gmail.com>
2024-10-17 12:03:11 +0200

    Release 1.1.3

--- wp-juggler-client.php
+++ wp-juggler-client.php
@@ -13,7 +13,7 @@
  * Plugin Name:       WP Juggler Client
  * Plugin URI:        https://wpjuggler.com
  * Description:       WP Juggler Client
- * Version:           1.1.2
+ * Version:           1.1.3
  * Author:            BoldThemes
  * Author URI:        https://wpjuggler.com
  * License:           GPL-3.0
